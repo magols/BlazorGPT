f7cf6f2c690164cc237fc4ca308a98ec2b7e9133
Magnus Olsson <magols@live.com>
2023-05-13 17:31:55 +0200

    Bumped Betalgo.OpenAI. Necessary EF migration since model expanded.

--- src/BlazorGPT/Migrations/20230513152916_messagename.Designer.cs
+++ src/BlazorGPT/Migrations/20230513152916_messagename.Designer.cs
@@ -4,6 +4,7 @@ using BlazorGPT.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlazorGPT.Migrations
 {
     [DbContext(typeof(BlazorGptDBContext))]
-    partial class BlazorGptDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230513152916_messagename")]
+    partial class messagename
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/BlazorGPT/Migrations/20230513152916_messagename.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorGPT.Migrations
+{
+    /// <inheritdoc />
+    public partial class messagename : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Messages",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Messages");
+        }
+    }
+}
