0d7a8f225a2a18a4f50425fbc779e1340c6b6faa
Magnus Olsson <magols@live.com>
2024-09-15 12:14:17 +0200

    Sample interceptors

---
+++ src/BlazorGPT/Pipeline/Interceptors/SampleInterceptor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.SemanticKernel;
+
+namespace BlazorGPT.Pipeline.Interceptors;
+
+public class SampleInterceptor(IServiceProvider serviceProvider) : InterceptorBase(serviceProvider), IInterceptor
+{
+    public override string Name { get; } = "Sample interceptor";
+
+    public override Task<Conversation> Send(Kernel kernel, Conversation conversation,
+        Func<string, Task<string>>? onComplete = null,
+        CancellationToken cancellationToken = default)
+    {
+        conversation.Messages.Last().Content += " [SampleInterceptor] send";
+        return Task.FromResult(conversation);
+    }
+
+    public override Task<Conversation> Receive(Kernel kernel, Conversation conversation,
+        Func<string, Task<string>>? onComplete = null,
+        CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(conversation);
+    }
+}
